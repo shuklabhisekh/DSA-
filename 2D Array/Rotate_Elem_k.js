92290ee90b6a3bb3e45a1dc34f154e43fc1a20c4
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-04-08 03:15:10 +0530

    practicing..

---
+++ 2D Array/Rotate_Elem_k.js
@@ -0,0 +1,9 @@
+// You are given an array of n elements and an integer k, you need to rotate the array by k units towards right direction ( => ).
+
+
+// Input
+
+let k=1;
+arr=[1,2,3]
+
+for(let i=k;i)
