c9cfe0c9894a2cbd9bf396d0eafc9c1f4773ac8e
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-05-20 22:02:21 +0530

    anagram

--- 2D Array/jf.js
+++ 2D Array/jf.js
@@ -1,9 +1,20 @@
 
-const store = {
-  x: "yellow",
-  z: () => {
-    return 5+5+"10";
-  },
-};
+const store = ["eat","tea","tan","nat","ate","bat"]
+let ans=[]
+let diff=[]
+for(let i=0;i<store.length;i++){
+  let index=arr[i]
+  if(i==0){
+ans.push(index[i])
+diff.push(index[i])
+  }
+  else{
+    for(let j=0;j<diff;j++){
+      let ind=diff[j]
+      for(let k=0;k<ind.length;k++){
+        
+      }
+    }
 
-console.log(store.z());
+  }
+}
