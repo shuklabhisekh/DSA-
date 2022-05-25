afd454bdf0358dfb2c68dca153560ba06cb2b15d
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-05-25 14:15:55 +0530

    async await

--- 2D Array/jf.js
+++ 2D Array/jf.js
@@ -1,20 +1,31 @@
+// const store = ["eat","tea","tan","nat","ate","bat"]
+// let ans=[]
+// let diff=[]
+// for(let i=0;i<store.length;i++){
+//   let index=arr[i]
+//   if(i==0){
+// ans.push(index[i])
+// diff.push(index[i])
+//   }
+//   else{
+//     // for(let j=0;j<diff;j++){
+//     //   let ind=diff[j]
+//     //   for(let k=0;k<ind.length;k++){
 
-const store = ["eat","tea","tan","nat","ate","bat"]
-let ans=[]
-let diff=[]
-for(let i=0;i<store.length;i++){
-  let index=arr[i]
-  if(i==0){
-ans.push(index[i])
-diff.push(index[i])
-  }
-  else{
-    // for(let j=0;j<diff;j++){
-    //   let ind=diff[j]
-    //   for(let k=0;k<ind.length;k++){
+//     //   }
+//     // }
 
-    //   }
-    // }
+//   }
+// }
 
-  }
+store();
+sec();
+
+async function store() {
+  let value = await "https://api.icndb.com/jokes/random";
+  console.log("1");
+}
+
+function sec() {
+  console.log(2);
 }
