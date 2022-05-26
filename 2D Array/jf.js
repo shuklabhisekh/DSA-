0acb14ac7d8fd80ad525a3be580f48610e0c4f21
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-05-26 08:34:21 +0530

    shallow

--- 2D Array/jf.js
+++ 2D Array/jf.js
@@ -29,7 +29,106 @@
 //   console.log(a);
 // }
 
-let arr = [1,2,4,9,5,6,7]
+// let arr = [1,2,4,9,5,6,7]
+// let a= 10
+// console.log(isNaN(a))
+// console.log(arr.slice(1)) //4,9,5,6
+// console.log(arr)
 
-console.log(arr.splice(2,4,10)) //4,9,5,6
-console.log(arr)
+
+// const randomNumbers = [4, 11, 42, 14, 39];
+// const filteredArray = randomNumbers.filter(n => {  
+//   return n > 5;
+// });
+
+// console.log(filteredArray,randomNumbers)
+
+
+// const arrayOfNumbers = [1, 2, 3, 4];
+ 
+// const sum = arrayOfNumbers.reduce((x,acc)=>{
+//   return acc + x
+// },100)
+ 
+// console.log(sum)
+
+
+
+// const finalParticipants = ['Taylor', 'Donald', 'Don', 'Natasha', 'Bobby'];
+ 
+// // add string after each final participant
+// const announcements = finalParticipants.map(member => {
+//   return member + ' joined the contest.';
+// })
+ 
+// console.log(announcements);
+
+// fun(1,2,4,5)
+
+// function fun(){
+//   console.log([...arguments])
+// }
+
+
+  // // SHALLOW EQUALITY
+  const source = {
+    name: "Abhishek",
+    detail: {
+      age: 20,
+      address: {
+        city: "Bangalore",
+        state: "Karnataka",
+      },
+    },
+  };
+
+  const target = {
+    name: "Abhishek",
+    detail: {
+      age: 20,
+      address: {
+        city: "Bangalore",
+        state: "Karnataka",
+      },
+    },
+  };
+
+
+  const deepCompare = (source, target) => {
+    let sourcekeys = Object.keys(source);
+    let targetkeys = Object.keys(target);
+    if(sourcekeys.length!=targetkeys.length){
+      return false
+    }
+
+    for( let i=0;i<sourcekeys.length;i++){
+      if(typeof source[sourcekeys[i]]=="object"){
+        if(!deepCompare(source[sourcekeys[i]],target[targetkeys[i]])){
+          return false
+        }
+      }
+      else{
+        if(source[sourcekeys[i]]!= target[targetkeys[i]]){
+          return false
+        }
+      }
+    }
+    return true;
+  };
+  console.log(deepCompare(source, target));
+  // const shallowCompare = (source, target) => {
+  //   const sourcekeys = Object.keys(source);
+  //   const targetkeys = Object.keys(target);
+  //   if (sourcekeys.length !== targetkeys.length) {
+  //     console.log(sourcekeys, targetkeys)
+  //     return false;
+
+  //   }
+  //   for (let i = 0; i < sourcekeys.length; i++) {
+  //     if (source[sourcekeys[i]] !== target[sourcekeys[i]]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // };
+  // console.log(shallowCompare(source, target));
