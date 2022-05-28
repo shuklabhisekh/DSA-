e09542f87f93d1d8a5f342933c562f3c911464bf
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-05-28 17:20:40 +0530

    subsequence

--- 2D Array/subsequence.js
+++ 2D Array/subsequence.js
@@ -1,20 +1,18 @@
+let arr = [1, 2, 3, 4];
 
-let arr= [1,2,3,4]
+let ans = [];
+let str = "";
 
-let ans=[]
-let str=""
+subsequence(arr, str, 0);
+function subsequence(arr, str, index) {
+  // console.log(ans,index,"check")
+  if (index == arr.length) {
+    ans.push(str);
+    return;
+  }
 
-subsequence(arr,str,0)
-function subsequence(arr,str,index){
-    
-    console.log(ans,index,"check")
-    if(index==arr.length){
-        ans.push(str)
-        return 
-    }
-
-    subsequence(arr, str+arr[index], index+1)
-    subsequence(arr,str,index+1)
-    
-
+  subsequence(arr, str + arr[index], index + 1);
+  subsequence(arr, str, index + 1);
 }
+
+console.log(ans)
