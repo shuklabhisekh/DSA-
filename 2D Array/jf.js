717b7ce6b27392265c5b56be89f66d817ca771ad
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-06-03 21:20:53 +0530

    subsequence

--- 2D Array/jf.js
+++ 2D Array/jf.js
@@ -156,3 +156,30 @@ function countChar(callback) {
     }
   }
 }
+
+function printSubsequences(arr, n)
+{
+
+    // Number of subsequences is (2**n -1)
+    let opsize = parseInt(Math.pow(2, n), 10);
+
+    // Run from counter 000..1 to 111..1
+    for(let counter = 1; counter < opsize; counter++)
+    { let store = ""
+        for(let j = 0; j < n; j++)
+        {
+
+            // Check if jth bit in the counter is set
+            // If set then print jth element from arr[]
+            if ((counter & (1 << j)) != 0)
+                // document.write(arr[j] + " ");
+               store = store + arr[j] + " "
+        }
+        console.log(store)
+    }
+}
+ 
+let arr = [ 1, 2, 3, 4 ];
+let n = arr.length;   
+
+printSubsequences(arr, n)
