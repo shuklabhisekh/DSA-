e8373de88d499538dd5cef766c1655c0668c0c29
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-06-22 11:24:06 +0530

    class

--- 2D Array/int.js
+++ 2D Array/int.js
@@ -1,6 +1,25 @@
-let arr = [1, 4];
+// function User(name, age) {
+//   (this.name = name), (this.age = age);
+// }
+// User.prototype.a = "a";
+// console.log(User.prototype);
 
-let ans = arr.map((ele) => {
-  return ele * 2;
-});
-console.log(ans);
+// const user1Moredetail = new User("abhishek", 20);
+// console.log(user1Moredetail);
+
+class User {
+  constructor(name, age, gender) {
+    (this.name = name), (this.age = age), (this.gender = gender);
+  }
+  findAge() {
+    console.log(`${this.name} is ${this.age} yrs old`);
+  }
+  findGender() {
+    console.log(`${this.name} is ${this.gender}`);
+  }
+}
+
+const aman = new User("aman", 23, "male");
+const jay = new User("jay", 18, "male");
+
+console.log(Object.getPrototypeOf(aman))
---
+++ 2D Array/js/proto.js
@@ -0,0 +1,9 @@
+
+const user1={
+    name:"Ajay",
+    age:20,
+    address: "My address"
+}
+
+const user1Moredetail = new Object.create(user1)
+console.log(user1Moredetail)
