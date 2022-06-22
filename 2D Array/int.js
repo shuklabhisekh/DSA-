4c57532fb5fe6506e836bae21be1b6d29b92971c
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-06-23 00:13:51 +0530

    checking function hoisting

--- 2D Array/int.js
+++ 2D Array/int.js
@@ -1,25 +1,9 @@
-// function User(name, age) {
-//   (this.name = name), (this.age = age);
-// }
-// User.prototype.a = "a";
-// console.log(User.prototype);
-
-// const user1Moredetail = new User("abhishek", 20);
-// console.log(user1Moredetail);
-
-class User {
-  constructor(name, age, gender) {
-    (this.name = name), (this.age = age), (this.gender = gender);
-  }
-  findAge() {
-    console.log(`${this.name} is ${this.age} yrs old`);
-  }
-  findGender() {
-    console.log(`${this.name} is ${this.gender}`);
-  }
+function func() {
+  console.log("function");
 }
+func();
 
-const aman = new User("aman", 23, "male");
-const jay = new User("jay", 18, "male");
 
-console.log(Object.getPrototypeOf(aman))
+var func = 2;
+
+console.log(func);
