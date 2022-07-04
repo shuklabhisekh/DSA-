d723d797a0948681b6a43d2370a5607a78c58c7c
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-07-04 18:34:28 +0530

    added registrer

--- 2D Array/Auth/src/controller/auth.controller.js
+++ 2D Array/Auth/src/controller/auth.controller.js
@@ -1,5 +1,20 @@
-const register = (req, res) => {
-  return res.send("Register");
+const jwt = require("jsonwebtoken");
+const User = require("../model/user.model");
+require("dotenv").config();
+// console.log(process.env.SECRET_KEY);
+const newToken = (user) => {
+  return jwt.sign({ user }, process.env.SECRET_KEY);
+};
+const register = async (req, res) => {
+  let user = await User.findOne({ email: req.body.email }).lean().exec();
+  if (user) {
+    return res.status(400).send({ message: "The email already exist" });
+  }
+  user = await User.create(req.body);
+
+  const token = newToken(user);
+
+  return res.send({ user, token });
 };
 
 const login = (req, res) => {
--- 2D Array/Auth/src/model/user.model.js
+++ 2D Array/Auth/src/model/user.model.js
@@ -1,5 +1,22 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
+var bcryptjs = require("bcryptjs");
 const UserSchema = new mongoose.Schema(
-    
-)
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: false,
+  }
+);
+
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  var hash = bcryptjs.hashSync(this.password, 8);
+  this.password = hash;
+  return next();
+});
+
+module.exports = mongoose.model("user", UserSchema);
